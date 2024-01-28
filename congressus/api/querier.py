ff123a64630c0ceefe83f3d8b1cf2c7d5179c8a0
s2191229 <n.e.djojomoenawie@student.utwente.nl>
2024-01-28 12:25:11 +0100

    Add support for POST requests and implement webhook creation

--- congressus/api/querier.py
+++ congressus/api/querier.py
@@ -1,9 +1,18 @@
 import requests
+from enum import Enum
 
 from api.base_client import BaseClient
 from models.group_membership import GroupMembership
 from models.member import Member
 from models.paginated_response import PaginatedResponse
+from models.webhook import ConceptualWebhook
+
+
+class HTTPMethod(Enum):
+    GET = 0
+    POST = 1
+    PUT = 2
+    DELETE = 3
 
 
 class Querier:
@@ -19,7 +28,14 @@ class Querier:
         self.domain = client.get_domain()
         self.auth_header = {"Authorization": f"Bearer {client.get_key()}"}
 
-    def authorized_request(self, path: str, params: dict = dict(), headers: dict = dict()) -> dict:
+    def authorized_request(
+        self,
+        path: str,
+        method: HTTPMethod = HTTPMethod.GET,
+        params: dict = dict(),
+        payload: dict = dict(),
+        headers: dict = dict(),
+    ) -> dict:
         """
         Returns the response to an HTTP request.
         Throws error on bad status code.
@@ -28,7 +44,14 @@ class Querier:
         url = self.domain + path
         headers.update(self.auth_header)
 
-        response = requests.get(url, params=params, headers=headers)
+        match method:
+            case HTTPMethod.GET:
+                response = requests.get(url, params=params, headers=headers)
+            case HTTPMethod.POST:
+                response = requests.post(url, json=payload, headers=headers)
+            case _:
+                raise ValueError("Unsupported HTTP method")
+
         response.raise_for_status()
 
         return response.json()
@@ -89,8 +112,19 @@ class WebhookQuerier(Querier):
         params = {"page": page, "page_size": page_size, "order": order}
         return PaginatedResponse(**self.authorized_request(path, params=params))
 
-    def create(self):
-        pass
+    def create(self, new: ConceptualWebhook):
+        path = self.BASE_PATH
+        payload = {
+            "url": new.url,
+            "headers": new.headers,
+            "signal": str(new.signal),
+            "technical_contact_email": new.technical_contact_email,
+            "http_basic_auth_enabled": new.http_basic_auth_enabled,
+        }
+        headers = {"Content-Type": "application/json"}
+        return PaginatedResponse(
+            **self.authorized_request(path, method=HTTPMethod.POST, payload=payload, headers=headers)
+        )
 
     def retrieve(self):
         pass
