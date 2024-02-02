01f1f548b450f524dfa80f47a35d529b58391ade
s2191229 <n.e.djojomoenawie@student.utwente.nl>
2024-02-02 13:35:17 +0100

    make PaginatedResponse attrs required

--- pygressus/api/paginated_response.py
+++ pygressus/api/paginated_response.py
@@ -4,16 +4,16 @@ from typing import Optional
 class PaginatedResponse:
     def __init__(
         self,
-        total=None,
-        has_prev=None,
-        prev_num=None,
-        has_next=None,
-        next_num=None,
-        data=None,
+        total,
+        has_prev,
+        prev_num,
+        has_next,
+        next_num,
+        data,
     ) -> None:
-        self.total: Optional[int] = total
-        self.has_prev: Optional[bool] = has_prev
-        self.prev_num: Optional[int] = prev_num
-        self.has_next: Optional[bool] = has_next
-        self.next_num: Optional[int] = next_num
-        self.data: Optional[list] = data
+        self.total: int = total
+        self.has_prev: bool = has_prev
+        self.prev_num: int = prev_num
+        self.has_next: bool = has_next
+        self.next_num: int = next_num
+        self.data: list = data
